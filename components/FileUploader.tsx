bd40422a37ee926582f0a549a0fff6f0e534dfea
Ron Barak <ronbarak2000@gmail.com>
2024-07-27 17:54:52 +0300

    drag n drop fixed

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -47,7 +47,7 @@ const FileUploader: React.FC<Props> = ({ files, onChange }) => {
       {isDragActive ? (
         <p>Drop the files here ...</p>
       ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
+        <p>Drag n drop some files here, or click to select files</p>
       )}
     </div>
   );
